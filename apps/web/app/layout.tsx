689b662373da5945c220ebef958c6d5879c5066f
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-11-11 23:50:41 +1100

    Added opengraph tags

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,18 +1,34 @@
+import { BASE_URL } from "@glimpz-io/config";
 import { AnalyticsProvider } from "@glimpz-io/hooks";
 import "@glimpz-io/ui/styles.css";
 import { GeistSans } from "geist/font";
 import type { Metadata } from "next";
 
+const TITLE = "Join The Wait List - Glimpz";
+const DESCRIPTION =
+    "Say goodbye to awkward encounters with Glimpz! Streamline your social interactions and never miss a chance to connect. No more overthinking, just genuine face-to-face connections.";
+const COLOR = "#f97316";
+const IMAGE = "https://i.imgur.com/DVFU6Yg.png";
+
 export const metadata: Metadata = {
-    title: "Join The Wait List - Glimpz",
-    description: "Say goodbye to awkward encounters with Glimpz! Streamline your social interactions and never miss a chance to connect. No more overthinking, just genuine face-to-face connections.",
+    metadataBase: new URL(BASE_URL),
+    title: TITLE,
+    description: DESCRIPTION,
+    themeColor: COLOR,
+    openGraph: {
+        title: TITLE,
+        description: DESCRIPTION,
+        images: IMAGE,
+        url: BASE_URL,
+    },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     const MIXPANEL_TOKEN = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN;
-    const FACEBOOK_ID = process.env.NEXT_PUBLIC_FACEBOOK_ID;
     if (!MIXPANEL_TOKEN) throw Error("missing mixpanel token");
-    else if (!FACEBOOK_ID) throw Error("missing facebook id");
+
+    const FACEBOOK_ID = process.env.NEXT_PUBLIC_FACEBOOK_ID;
+    if (!FACEBOOK_ID) throw Error("missing facebook id");
 
     return (
         <html lang="en" className="bg-gradient-to-r from-neutral-950 to-zinc-950 min-w-fit">
