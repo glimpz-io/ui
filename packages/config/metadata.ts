df1ec641bd5853167814d040febd88bb44856598
Ben Osborn <bengrantalbertosborn@gmail.com>
2023-11-21 22:50:40 +1100

    Updated meta image

--- packages/config/metadata.ts
+++ packages/config/metadata.ts
@@ -4,5 +4,5 @@ export const META_TITLE = "Join The Waitlist - Glimpz";
 export const META_DESCRIPTION =
     "Turbocharge your lead generation and conversions at in-person networking events, effortlessly follow-up on connections, and ditch annoying business cards. Join our waitlist today!";
 export const META_COLOR = "#0ea5e9";
-export const META_IMAGE = "https://i.imgur.com/9sw4AUN.png";
+export const META_IMAGE = "https://i.imgur.com/szcMlMo.png";
 export const META_URL = BASE_URL;
